c97e5e1bb9e849886f228f09fc076bb92c7799cf
Zaid Sheikh <zsheikh@cs.cmu.edu>
2023-07-27 15:07:41 -0400

    conda fix for dockerfile setup.sh

--- dockerfiles/setup.sh
+++ dockerfiles/setup.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -i
 
+eval "$(conda shell.bash hook)"
 conda create -n qaware-decode python=3.9.13
+conda activate qaware-decode
 pip install clearml
 conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
 cd /
